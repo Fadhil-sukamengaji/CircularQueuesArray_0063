800ec4e5340163c5c405a23050a9b7feca81c65f
FadhilM <mudzakifadhil@gmail.com>
2025-06-12 17:44:49 +0700

    public void  insert

--- circullar.cpp
+++ circullar.cpp
@@ -10,5 +10,43 @@ class Queues
         static const int max = 5;
         int FRONT, REAR;
         int queue_array[5];
-        
-}
+
+    public:
+        Queues()
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+
+        void insert()
+        {
+            int num;
+            cout << "enter a number : ";
+            cin >> num;
+            cout << endl;
+
+            //1.cek apakah antrian penuh
+            if((FRONT == 0 && REAR == max - 1) || (FRONT == REAR +1))
+            {
+                cout << "\nQueue overflow\n"; 
+                return;
+            }
+
+            //2.cek apakah antrian kosong
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                //jika rear berada di posisi terakhir array, kembali ke awal array
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = num;
+        }
+
+};
