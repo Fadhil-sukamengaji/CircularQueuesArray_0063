ab99be09b8637191d9ad71b0b3ce9cd35ec1fc51
FadhilM <mudzakifadhil@gmail.com>
2025-06-12 18:29:25 +0700

    VOID DISPLAY

--- circullar.cpp
+++ circullar.cpp
@@ -86,6 +86,15 @@ class Queues
                 return;
             }
 
-            
+            cout << "\nelements in the queue are...\n";
+
+            if (FRONT_position <= REAR_position)
+            {
+                while (FRONT_position <= REAR_position)
+                {
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+            }
         }
 };
